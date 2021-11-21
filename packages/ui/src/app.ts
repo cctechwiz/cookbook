bc775bdef9c48cb4da45f06045de8b505dc6165a
Josh Maxwell <cctechwiz@gmail.com>
2021-11-20 20:32:53 -0700

    Fixing build order and dependencies

---
+++ packages/ui/src/app.ts
@@ -0,0 +1,12 @@
+import { Recipe } from '@cookbook/shared';
+
+const recipe: Recipe = {
+  id: "id",
+  owner: "jmaxwell",
+  title: "one",
+  ingredients: [],
+  directions: [],
+  categories: []
+};
+console.log(recipe);
+
